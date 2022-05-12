9340b2f9fcf009d69c9ec12108a36f97c9cf99e6
Raphael Oliveira <raphaaaodev@gmail.com>
2022-05-11 21:11:00 -0300

    make search module more specific

--- module/search.py
+++ module/search.py
@@ -4,63 +4,26 @@ import os
 
 
 class Search():
-    def separate_processos(txt_path):
-        processos_list = []
-        with open(txt_path,"rt",encoding="utf-8") as txt_file:
-            lines = txt_file.readlines()
-            processo_separators = [index for index, line in enumerate(lines) if "--------------" in line]
-
-            for index, separator in enumerate(processo_separators):
-                if index == len(processo_separators)-1:
-                    continue
-
-                index_processo_title = separator+1
-                index_first_line_processo_body = separator+2
-                index_last_line_processo_body = processo_separators[index+1]-1
-
-                string = ""
-
-                for line in lines[index_first_line_processo_body:index_last_line_processo_body]:
-                    string += line.rstrip().lower().replace(" ","")
-
-
-                processo = {lines[index_processo_title].rstrip():{
-                    "to_search" : string,
-                    "to_export" : lines[index_first_line_processo_body:index_last_line_processo_body]
-                }}
-
-                processos_list.append(processo)
-
-        return processos_list
 
     #def export_needed_processos(processos_dictionary, txt_file_with_laywers):
-    def export_needed_processos(processos_list):
+    def export_needed_processos(all_processos_list):
+        name = "MAXIMILIANO NAGL GARCEZ"
         search = "MAXIMILIANO NAGL GARCEZ".lower().replace(" ","")
-        processos_of_search = {}
         processos_found = []
+        print("export_needed_processos")
+        print(all_processos_list)
+        txt_folder_path = os.sep.join([f'assets\\{all_processos_list[0][0]["date"]}', search.replace(" ","-").title()])
 
-        txt_folder_path = os.sep.join(['assets\\processos-coletados', search.replace(" ","-")])
-
-        for processo in processos_list:
-            processo_key = next(iter(processo))
-            
-            text = processo[processo_key]['to_search']
-            if search in text:
-                processo_name = processo_key.replace("Processo Nº ","")
-                print(processo_name)
-                processos_found.append(processo_name)
+        for processos_list in all_processos_list:
+            for processo in processos_list:
+                processo_key = next(iter(processo))
+                
+                text = processo[processo_key]['to_search']
+                if search in text:
+                    processo_name = processo_key.replace("Processo Nº ","")
+                    print(processo_name)
+                    processos_found.append(processo_name)
 
         print(len(processos_found))
 
-        
-        #return txt_path
-
-                            
-    def separate_processos_from_list(txt_path_list):
-        txt_files = []
-
-        for txt_path in txt_path_list:
-            txt_file = Search.separate_processos(txt_path)
-            txt_files.append(txt_file)
-
-        return txt_files
+        return processos_found
