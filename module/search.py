73575bfedf1a70b5d7299db9e92ddfd6f3e3d714
Raphael Oliveira <raphaaaodev@gmail.com>
2022-05-12 00:08:05 -0300

    Unify txt_to_searchable.py to optimize performance

--- module/search.py
+++ module/search.py
@@ -1,29 +1,87 @@
 from concurrent.futures import process
 from pprint import pprint
+from pathlib import Path
 import os
+from time import sleep
+
+from .addons import Dirs_files
 
 
 class Search():
 
     #def export_needed_processos(processos_dictionary, txt_file_with_laywers):
-    def export_needed_processos(all_processos_list):
+    def export_needed_processos(txt_files):
         name = "MAXIMILIANO NAGL GARCEZ"
-        search = "MAXIMILIANO NAGL GARCEZ".lower().replace(" ","")
-        processos_found = []
-        print("export_needed_processos")
-        print(all_processos_list)
-        txt_folder_path = os.sep.join([f'assets\\{all_processos_list[0][0]["date"]}', search.replace(" ","-").title()])
+        search = name.lower().replace(" ","")
 
-        for processos_list in all_processos_list:
-            for processo in processos_list:
-                processo_key = next(iter(processo))
-                
-                text = processo[processo_key]['to_search']
-                if search in text:
-                    processo_name = processo_key.replace("Processo Nº ","")
-                    print(processo_name)
-                    processos_found.append(processo_name)
+        everything_txt_creation_flag = False
 
-        print(len(processos_found))
+        export_folders = []
 
-        return processos_found
+        #Dirs_files.append_new_line(everything_txt,name)
+        
+
+        for txt_file in txt_files:
+            
+            txt_path = Path(txt_file)
+            orgao = txt_path.stem
+            date = txt_path.parent.name
+            lawyer_name = name.replace(" ","-").title()
+
+            exported_search_path = Path(f'assets\\processos-exportados\\{lawyer_name}\\{date}\\{orgao}')
+
+            processos_names_txt = Path(exported_search_path) / "!Todos-os-processos.txt"
+            processos_names = []
+
+            if (processos_names_txt.exists()):
+                processos_names_txt.unlink()
+
+            export_folders.append(exported_search_path)
+
+            isExist = os.path.exists(exported_search_path)
+            if not isExist:
+                os.makedirs(exported_search_path)
+
+            with open(txt_file,"rt",encoding="utf-8") as txt_file:
+                lines = txt_file.readlines()
+                processo_separators = [index for index, line in enumerate(lines) if "--------------" in line]
+
+                for index, separator in enumerate(processo_separators):
+                    if index == len(processo_separators)-1:
+                        continue
+
+                    index_processo_title = separator+1
+                    index_first_line_processo_body = separator+2
+                    index_last_line_processo_body = processo_separators[index+1]-1
+
+                    string = ""
+
+                    for line in lines[index_first_line_processo_body:index_last_line_processo_body]:
+                        string += line.rstrip().lower().replace(" ","")
+
+                    if search in string:
+                        processo_name = lines[index_processo_title].rstrip().replace("Processo Nº ","")
+                        processo_txt_path = Path(exported_search_path) / f"{processo_name}.txt"
+                        
+                        processos_names.append(processo_name+"\n")
+                        #Dirs_files.append_new_line(everything_txt,f"\t{processo_name}")
+
+                        print(processo_name)
+
+                        if (processo_txt_path.exists()):
+                            processo_txt_path.unlink()
+
+                        with open(processo_txt_path, "w",encoding='utf-8') as file_object:
+                            file_object.writelines(lines[index_first_line_processo_body:index_last_line_processo_body])
+
+            if len(processos_names)==0:
+                exported_search_path.rmdir()
+                continue
+
+            #Dirs_files.append_new_line(everything_txt,f"\n")
+
+            with open(processos_names_txt, "w",encoding='utf-8') as file_object:
+                file_object.write(f"Total: {len(processos_names)}\n")
+                file_object.writelines(processos_names)
+
+        return export_folders
